006f5ac0541a57722c2967e9fafaa36aaf23b7c4
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-04-01 04:20:48 +0300

    Add project files.

---
+++ Calculator/BasicCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+   public class BasicCalculator // Componet Base Programing icin .dll ile farkli platformlara paylasmamda kullanabilirim.
+        
+    {
+        public void Sum(int[] arr)
+        {
+            int sum = 0;
+            foreach (var result in arr)
+            {
+                sum += result;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticeNetProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeNetProject
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            BasicCalculator basicCalculator = new BasicCalculator();
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            int[] numbers2 = new int[] { 10, 20, 30, 40, 50, 60 };
+
+            basicCalculator.Sum(numbers);
+            basicCalculator.Sum(numbers2);
+
+
+            
+
+        }
+
+
+    }
+}
